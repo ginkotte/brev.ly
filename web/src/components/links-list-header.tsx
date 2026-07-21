fcf287adf24883e71f480d936facad2661128a20
Gustavo Inkotte <gus.inkotte@gmail.com>
2026-07-20 23:27:19 -0300

    fix: Fix Links List header title

--- web/src/components/links-list-header.tsx
+++ web/src/components/links-list-header.tsx
@@ -4,7 +4,7 @@ import { DownloadSimpleIcon } from "@phosphor-icons/react";
 export function LinksListHeader() {
   return(
     <div className="w-full flex items-center justify-between">
-      <span className="text-lg font-bold text-gray-600">Meus Links</span>
+      <span className="text-lg font-bold text-gray-600">Meus links</span>
         <Button variant="secondary" className="h-8 rounded-sm" >
           <DownloadSimpleIcon strokeWidth={1.5} className="size-4"/>
           <span className="text-sm">Baixar CSV</span>
