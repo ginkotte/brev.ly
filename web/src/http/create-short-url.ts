8a26ff719e04b153fa8b94d58214461cc1376c75
Gustavo Inkotte <gus.inkotte@gmail.com>
2026-08-09 22:03:52 -0300

    feat: Http request to create short url

---
+++ web/src/http/create-short-url.ts
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+interface CreateShortUrlParams {
+  alias: string,
+  originalUrl: string
+}
+
+export async function createShortUrl({alias, originalUrl}: CreateShortUrlParams) {
+  const data = {
+    alias,
+    originalUrl
+  }
+
+  const response = await axios.post<{url: string}>('http://localhost:3333/url', data)
+
+  return {url: response.data.url}
+}
